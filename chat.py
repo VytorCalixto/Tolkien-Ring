3f7ff9110338ce34f42b207a03393799e549d738
VytorCalixto <vytorcalixto@gmail.com>
2015-11-04 22:47:34 -0200

    Get the next node in the token ring
    But does nothing, just get the IP

--- chat.py
+++ chat.py
@@ -6,11 +6,15 @@ host = socket.gethostbyname(hostname)
 print "Você está na máquina %s, IP %s" % (hostname, host)
 port = 1337
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 s.bind((host, port))
 print "Servidor rodando na porta %d" % port
+
+nextHostname = raw_input("Em qual máquina você quer se conectar?\n")
+nextHost = socket.gethostbyname(nextHostname)
+print nextHost
+
 while 1:
     message, addr = s.recvfrom(1024) # 1024 é o tamanho do buffer
     if message:
         print addr, ">:", message
-        s.sendto(hostname + " responde: " + message, addr) # replica a mensagem
+        #s.sendto(hostname + " responde: " + message, addr) # replica a mensagem
