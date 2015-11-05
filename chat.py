8ab9bfdcab85c07019cff2df144623154a265c75
VytorCalixto <vytorcalixto@gmail.com>
2015-11-05 00:52:54 -0200

    Terrible example of curses for the interface
    Related: #1

--- chat.py
+++ chat.py
@@ -1,20 +1,54 @@
 # -*- coding:utf-8 -*-
 import socket
+import curses
+import curses.textpad
+import traceback
 
-hostname = socket.gethostname()
-host = socket.gethostbyname(hostname)
-print "Você está na máquina %s, IP %s" % (hostname, host)
-port = 1337
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind((host, port))
-print "Servidor rodando na porta %d" % port
+def printMessages(screen, messages):
+    yx = screen.getmaxyx()
+    topRange = len(messages) if ((yx[0]-2) > len(messages)) else (yx[0]-2)
+    for i in range(0, topRange):
+        screen.addstr(i+1, 1, messages[i])
 
-nextHostname = raw_input("Em qual máquina você quer se conectar?\n")
-nextHost = socket.gethostbyname(nextHostname)
-print nextHost
+def main(stdscr):
+    hostname = socket.gethostname()
+    host = socket.gethostbyname(hostname)
+    port = 1337
 
-while 1:
-    message, addr = s.recvfrom(1024) # 1024 é o tamanho do buffer
-    if message:
-        print addr, ">:", message
-        #s.sendto(hostname + " responde: " + message, addr) # replica a mensagem
+    stdscr.nodelay(1)
+    stdscr.addstr(0, 0, "Máquina: %s" % (hostname))
+    stdscr.addstr(1, 0, "IP: %s" % (host))
+    stdscr.addstr(2, 0, "Status: Conectando...")
+    stdscr.refresh()
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind((host, port))
+    
+    stdscr.clear()
+    yx = stdscr.getmaxyx()
+    stdscr.addstr(0, 0, "Máquina: %s" % (hostname))
+    stdscr.addstr(1, 0, "IP: %s:%d" % (host, port))
+    stdscr.addstr(2, 0, "Status: Conectado")
+    title = "### TOLKIEN RING ###"
+    stdscr.addstr(3, (yx[1] - len(title))/2, title, curses.A_REVERSE)
+    stdscr.refresh()
+    
+    # subscreen do chat
+    subscreen = stdscr.subwin(yx[0]-6, yx[1]-1, 4, 0)
+    subscreen.box()
+    subscreen.refresh()
+
+    messages = []
+
+    while True:
+        message, addr = s.recvfrom(1024) # 1024 é o tamanho do buffer
+        if message:
+            name = socket.gethostbyaddr(addr[0])[0].split('.')[0]
+            messages.append("%s: %s" % (name, message))
+            printMessages(subscreen, messages)       
+            subscreen.box()
+            subscreen.refresh()
+        
+
+if __name__=='__main__':
+    curses.wrapper(main)
