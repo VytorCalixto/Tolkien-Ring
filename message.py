6c2e53ec02fcc893bdbaeb981c7e5e109317d70b
VytorCalixto <vytorcalixto@gmail.com>
2015-11-07 12:20:20 -0200

    Function to set received and set read a message

--- message.py
+++ message.py
@@ -1,12 +1,12 @@
 class Message(object):
     def __init__(self):
-        self.control = ""
-        self.origin = ""
-        self.destiny = ""
+        self.control = ''
+        self.origin = ''
+        self.destiny = ''
         self.size = 0
-        self.data = ""
-        self.parity = ""
-        self.response = str(chr(0))
+        self.data = ''
+        self.parity = ''
+        self.response = chr(0)
     def setControl(self, control):
         self.control = control
     def setOrigin(self, origin):
@@ -20,7 +20,20 @@ class Message(object):
     def calcParity(self, data):
         pass # TODO: calcular paridade
     def setResponse(self, response):
-        self.response = str(chr(response))
+        self.response = chr(response)
+    def setReceived(self, machine):
+        # machine é o número da máquina no anel (0, 3)
+        # O bit mais a esquerda diz que a msg foi recebida
+        # O bit mais a direita diz que a msg foi aceita
+        # |Resposta|
+        # |AABBCCDD|
+        response = list(self.getResponse())
+        response[machine*2] = '1'
+        self.response = chr(int(''.join(response), 2))
+    def setRead(self, machine):
+        response = list(self.getResponse())
+        response[machine*2 + 1] = '1'
+        self.response = chr(int(''.join(response), 2))
     def getControl(self):
         return self.control
     def getOrigin(self):
@@ -33,8 +46,11 @@ class Message(object):
         return self.data
     def getParity(self):
         return self.parity
+    def checkParity(self);
+        # TODO: checar paridade
+        pass
     def getResponse(self):
-        return ord(self.response)
+        return '{0:08b}'.format(ord(self.response))
     def makeHandshake(self, origin, destiny, port):
         self.origin = origin
         self.destiny = destiny
